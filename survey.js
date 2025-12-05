26fae30ba1b28053467010aaa5233db91bbaaa9a
Dioni <dionisgggedeshi@gmail.com>
2025-12-05 14:59:03 +0100

    Update 01

---
+++ survey.js
@@ -0,0 +1,9 @@
+const submit = document.querySelector('#submit');
+
+submit.addEventListener('mouseover', function () {
+    submit.style.backgroundColor = '#277231e5';
+});
+
+submit.addEventListener('mouseout', function () {
+    submit.style.backgroundColor = '#258d33';
+});
